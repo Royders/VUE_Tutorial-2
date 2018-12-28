487c68dfefa4b55e75bb4a943171f30ec64b420b
Thomas Liebberger <thomas.liebberger@bcc.gmbh>
2018-12-28 20:45:57 +0100

    initial commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<template>
+<div> This is the header!</div>
+
+</template>
+
+<script>
+export default {
+    name: 'AppHeader',
+   
+}
+</script>
+
+
+<style>
+
+</style>
+
+
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import vue from "vue"
+import App from './App'
+
+
+new vue ({
+    render : h => h(App)
+}).$mount('#app')
